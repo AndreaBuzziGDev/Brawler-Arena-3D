b46d87757b4dee48d01b371054176a8d9564ea62
Andrea Buzzi <aby.development@outlook.it>
2024-05-10 01:48:25 +0200

    Creating Scriptable Object Data

--- Assets/Scripts/Game Logic/Player/PlayerCharacterData.cs
+++ Assets/Scripts/Game Logic/Player/PlayerCharacterData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Player Parameters", menuName = "Player Data/Player Params")]
 public class PlayerCharacterData : ScriptableObject
 {
     //TODO: EMBELLISH
