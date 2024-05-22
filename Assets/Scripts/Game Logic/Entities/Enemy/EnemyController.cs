50f1b97b7b3d0a9fff828d5e9e93ea2f4e9a0869
Andrea Buzzi <aby.development@outlook.it>
2024-05-22 21:27:48 +0200

    Evolved the controller to handle movement on the navmesh

--- Assets/Scripts/Game Logic/Entities/Enemy/EnemyController.cs
+++ Assets/Scripts/Game Logic/Entities/Enemy/EnemyController.cs
@@ -1,14 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : EntityWithHealth
 {
-    //TODO: DEFINE EXTENSIONS OF EntityWithHealth
+    private Transform target;
+    private NavMeshAgent myNavAgent;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        myNavAgent = GetComponent<NavMeshAgent>();
+        target = GameController.Instance.GetPlayerAnywhere.transform;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+        
+        //TODO: ALT SOLUTION FOR THIS?
+        if(GameController.Instance.IsPaused)
+            myNavAgent.destination = transform.position;
+        else
+            myNavAgent.destination = target.position;
+    }
+
+
+    //FUNCTIONALITIES
     public override void HandleDeath()
     {
         //TODO: DEVELOP
         Debug.Log("EnemyController - Handle Death - TODO DEVELOP");
     }
-
 }
