d66f542630e7fb933ca4987d0f586e32f5e41c8a
Andrea Buzzi <aby.development@outlook.it>
2024-05-24 22:26:43 +0200

    WIP implementing solution

--- Assets/Scripts/Game Logic/Entities/Enemy/EnemyController.cs
+++ Assets/Scripts/Game Logic/Entities/Enemy/EnemyController.cs
@@ -2,23 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//TODO: MIGHT NEED FURTHER RAMIFICATION WITH SEVERAL LEVELS OF ABSTRACTION TO HANDLE DIFFERENT WEAPON TYPES
+//TODO: THIS IS MEANT AS SUICIDE BOMBER ENEMY
 public class EnemyController : EntityWithHealth
 {
     //DATA
-    //WEAPONS HERE?
+    [SerializeField] WeaponController enemyWeapon;
+
+
+    //ON COLLISION
 
 
     //FUNCTIONALITIES
+
+
+    //WEAPON OPERATE
+
+
+    //EntityWithHealth CONCRETIZATION
     public override void HandleDeath()
     {
         Debug.Log("EnemyController - HandleDeath - TODO: PARTICLE AND OTHER DEATH STUFF");
         Destroy(gameObject);
     }
-
-    //ON COLLISION
-
-
-    //WEAPON OPERATE
     
 
 }
