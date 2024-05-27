9f08397802b1333708a40e41fdf5983047365847
Andrea Buzzi <aby.development@outlook.it>
2024-05-27 23:49:32 +0200

    Still WIP on structure for spawning enemies, this class will NOT compile at the moment (intentionally)

--- Assets/Scripts/Main Controllers/Events/SpawnEntityEventArgs.cs
+++ Assets/Scripts/Main Controllers/Events/SpawnEntityEventArgs.cs
@@ -6,21 +6,17 @@ using System;
 public class SpawnEntityEventArgs : ManagedEvent
 {
     //DATA
-    private SpawnPoint.SpawnPointType eventType;
-    public SpawnPoint.SpawnPointType EventType { get { return eventType; } }
-
     private int spawnPointInstanceID;
     public int SpawnPointInstanceID { get { return spawnPointInstanceID; } }
 
 
     //CONSTRUCTOR
-    public SpawnEntityEventArgs(SpawnPoint.SpawnPointType eventType, int spawnPointInstanceID)
+    public SpawnEntityEventArgs(int spawnPointInstanceID, )
     {
-        this.eventType = eventType;
         this.spawnPointInstanceID = spawnPointInstanceID;
     }
 
 
     //ABSTRACT CONCRETIZATION
-    public override int EventId { get { return (int) eventType; } }
+    public override int EventId { get { return (int) spawnPointInstanceID; } }
 }
