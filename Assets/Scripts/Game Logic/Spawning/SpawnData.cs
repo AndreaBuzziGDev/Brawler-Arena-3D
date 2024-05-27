7721a005a6e0269455185d9b28fbb3d75f296b19
Andrea Buzzi <aby.development@outlook.it>
2024-05-28 00:12:58 +0200

    Minor improvement and modifying comment

--- Assets/Scripts/Game Logic/Spawning/SpawnData.cs
+++ Assets/Scripts/Game Logic/Spawning/SpawnData.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class SpawnData
 {
     //DATA
-    //TODO: USE A SCRIPTABLEOBJECT TO CARRY PREFABS & OTHER THINGS
-    public GameObject TargetPrefab { get; }
+    //TODO: SOME CLASS (NOT NECESSARILY THIS ONE) MIGHT MAKE GOOD USE OF A SCRIPTABLEOBJECT TO CARRY PREFABS & OTHER THINGS
+    public EntityWithHealth TargetPrefab { get; }
     public int Quantity { get; }
 
 
     //CONSTRUCTOR
-    public SpawnData(GameObject targetPrefab, int quantity)
+    public SpawnData(EntityWithHealth targetPrefab, int quantity)
     {
         this.TargetPrefab = targetPrefab;
         this.Quantity = quantity;
