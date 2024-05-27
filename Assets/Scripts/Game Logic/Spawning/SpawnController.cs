ccc4ad348c5c82f4466a97fb24b4b5f4e4cfafd3
Andrea Buzzi <aby.development@outlook.it>
2024-05-28 00:49:54 +0200

    Added Spawner controller, but wip for other stuff

---
+++ Assets/Scripts/Game Logic/Spawning/SpawnController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    //DATA
+    [SerializeField] List<SpawnPoint> spawnPoints = new();
+
+
+    //LIFECYCLE FUNCTIONS
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //FUNCTIONALITIES
+
+    
+}
