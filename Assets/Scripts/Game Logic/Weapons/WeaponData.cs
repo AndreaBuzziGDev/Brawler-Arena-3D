7dd57e5a96608f5bbfa9465a08e4b6185021ff03
Andrea Buzzi <aby.development@outlook.it>
2024-06-17 20:28:36 +0200

    Removed menu redundancy

--- Assets/Scripts/Game Logic/Weapons/WeaponData.cs
+++ Assets/Scripts/Game Logic/Weapons/WeaponData.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Weapon Data", menuName = "Gameplay Data/Weapon Data")]
 public abstract class WeaponData : ScriptableObject
 {
     //TODO: EMBELLISH
