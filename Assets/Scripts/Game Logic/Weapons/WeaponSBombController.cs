d47420b1e54203d78e52de22147ee21113e0bb2f
Andrea Buzzi <aby.development@outlook.it>
2024-06-23 17:51:25 +0200

    Integrated new utility and optimized code

--- Assets/Scripts/Game Logic/Weapons/WeaponSBombController.cs
+++ Assets/Scripts/Game Logic/Weapons/WeaponSBombController.cs
@@ -20,30 +20,19 @@ public class WeaponSBombController : WeaponController
         {
             //FIND ALL HITTABLES IN A RADIUS
             //TODO: POLISH AND/OR EXTRACT AS A UTILITY OR OTHER KIND OF FUNCTIONALITY (LIKELY TO BE USED ELSEWHERE)
-            List<IHittable> hittables = new();
-            PlayerController pc = null;
-            Collider[] hitColliders = Physics.OverlapSphere(transform.position, wData.EffectiveRadius);
-            foreach (var hitCollider in hitColliders)
-            {
-                IHittable hitOther = hitCollider.gameObject?.GetComponent<IHittable>();
-                if(hitOther != null)
-                {
-                    if(hitOther is PlayerController controller)
-                        pc = controller;
-                    else if(wData.HasFriendlyFire)
-                        hittables.Add((IHittable) hitOther);
-                }
-            }
+            UtilsDetection.DetectionInfos dInfo = UtilsDetection.DetectColliders(transform.position, wData.EffectiveRadius);
             
             //DAMAGE INSTANCE
             DamageInstance dInstance = new DamageInstance(WData);
 
-            //DAMAGE DEALING
-            if(pc != null)
-                pc.HandleHit(dInstance);
-
-            foreach(IHittable hitbl in hittables)
-                hitbl.HandleHit(dInstance);
+            //DAMAGE DEALING - PLAYER
+            if(dInfo.Player != null)
+                dInfo.Player.HandleHit(dInstance);
+            
+            //DAMAGE DEALING - FRIENDLY FIRE
+            if(wData.HasFriendlyFire)
+                foreach(IHittable hitbl in dInfo.Hittables)
+                    hitbl.HandleHit(dInstance);
             
             //SELF-DESTRUCT
             if(ownerEntity) 
