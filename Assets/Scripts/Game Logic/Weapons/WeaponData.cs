f99ded7b0f87afc08d4db194ea384ee201f38498
Andrea Buzzi <aby.development@outlook.it>
2024-06-25 02:35:01 +0200

    Embellishment

--- Assets/Scripts/Game Logic/Weapons/WeaponData.cs
+++ Assets/Scripts/Game Logic/Weapons/WeaponData.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 
 public abstract class WeaponData : ScriptableObject
 {
-    //TODO: EMBELLISH
-    //TODO: DAMAGE TYPE?
-
-
     //DAMAGE
+    [Tooltip("The damage dealt by operating this weapon")]
+    [Range(0, 100)]
     [SerializeField] float damageAmount = 1;
     public float DamageAmount => damageAmount;
 
     //OWNERSHIP
+    [Tooltip("If checked, this weapon needs to be linked to an owner (EG: self-destruction)")]
     [SerializeField] bool needsOwnerToOperate = false;
     public bool NeedsOwnerToOperate { get { return needsOwnerToOperate; } }
 
     //FRIENDLY FIRE
+    [Tooltip("Used for enemies. If checked, this weapon can damage other enemies")]
     [SerializeField] bool hasFriendlyFire = false;
     public bool HasFriendlyFire { get { return hasFriendlyFire; } }
 
--- Assets/Scripts/Game Logic/Weapons/WeaponSBombData.cs
+++ Assets/Scripts/Game Logic/Weapons/WeaponSBombData.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Weapon Data", menuName = "Gameplay Data/Weapon Data/Self-Destruct")]
 public class WeaponSBombData : WeaponData
 {
-    //TODO: EMBELLISH
+    [Tooltip("The radius of the explosion in units")]
+    [Range(0, 50)]
     [SerializeField] float effectiveRadius = 10;
     public float EffectiveRadius => effectiveRadius;
 
