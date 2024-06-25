63d493b3a4ec429dd09c61843b20b4954f8bf5e2
Andrea Buzzi <aby.development@outlook.it>
2024-06-25 02:40:06 +0200

    Minor Comment Improvements

--- Assets/Scripts/Game Logic/Weapons/WeaponRangedData.cs
+++ Assets/Scripts/Game Logic/Weapons/WeaponRangedData.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Weapon Data", menuName = "Gameplay Data/Weapon Data/Ranged")]
 public class WeaponRangedData : WeaponData
 {
-    //PROJECTILE DATA
+    //SPEED
     [Tooltip("The speed module of the projectile")]
     [Range(0, 100)]
     [SerializeField] float projectileSpeed = 10;
     public float ProjectileSpeed => projectileSpeed;
 
 
+    //LIFETIME
     [Tooltip("The duration in second before the projectile vanishes")]
     [Range(0, 20)]
     [SerializeField] float maxLifetime = 10;
