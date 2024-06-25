f93583befe8fd4c7924e4d9616c4195c6f6b95a0
Andrea Buzzi <aby.development@outlook.it>
2024-06-25 03:02:04 +0200

    Polished Data and added embellishment

--- Assets/Scripts/Game Logic/Entities/EntityData.cs
+++ Assets/Scripts/Game Logic/Entities/EntityData.cs
@@ -5,29 +5,33 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Entity Parameters", menuName = "Entity Data/Entity Params")]
 public class EntityData : ScriptableObject
 {
-    //TODO: EMBELLISH
-    //1) DEFINE RANGES MIN AND MAX
-    //2) DEFINE STEP
 
     //HEALTH
+    [Tooltip("The maximum amount of damage that can be absorbed after being shielded.")]
+    [Min(0)]
     [SerializeField] int maxHealth = 5;
     public int MaxHealth => maxHealth;
 
-    //SHIELD
+
+    //SHIELD CHARGE
+    [Tooltip("Shield absorbs damage before health. Any damage instance with at least 1 shield charge is completely denied.")]
+    [Min(0)]
     [SerializeField] int maxShield = 5;
-    [SerializeField] float shieldRechargeRate = 1;
-    [SerializeField] int shieldCooldownTimer = 7;
-
     public int MaxShield => maxShield;
+
+
+    //SHIELD RECHARGE RATE
+    [Tooltip("The amount of shield recharged per second.")]
+    [Min(0)]
+    [SerializeField] float shieldRechargeRate = 1;
     public float ShieldRechargeRate => shieldRechargeRate;
+
+
+    //SHIELD RECHARGE DELAY
+    [Tooltip("The amount of time before the shield starts recharging.")]
+    [Min(0)]
+    [SerializeField] int shieldCooldownTimer = 7;
     public int ShieldCooldownTimer => shieldCooldownTimer;
-
-
-    //MOVEMENT
-    //TODO: REMOVE. SPEED IS HANDLED IN ANOTHER WAY ENTIRELY.
-    [SerializeField] float movementSpeed = 5;
-    public float MovementSpeed => movementSpeed;
-
 
     //...
 
