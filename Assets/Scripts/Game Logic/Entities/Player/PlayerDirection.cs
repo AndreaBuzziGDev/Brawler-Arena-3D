35d0266a648f56ae1296760819574372316d0a0c
Andrea Buzzi <aby.development@outlook.it>
2024-06-25 02:48:42 +0200

    Not really a good strategy.

--- Assets/Scripts/Game Logic/Entities/Player/PlayerDirection.cs
+++ Assets/Scripts/Game Logic/Entities/Player/PlayerDirection.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//TODO: EVALUATE IMPROVING THIS BY USING A REQUIRED OR SOMETHING ELSE?
 public class PlayerDirection : MonoBehaviour
 {
     //INSPECTOR REFERENCES
