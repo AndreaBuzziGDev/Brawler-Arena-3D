e450353c2407692b3713bff7ca32053fe0b1b607
Andrea Buzzi <aby.development@outlook.it>
2024-09-11 00:12:35 +0200

    Removed unnecessary commented part

--- Assets/Scripts/Main Controllers/ParticleManager.cs
+++ Assets/Scripts/Main Controllers/ParticleManager.cs
@@ -35,28 +35,4 @@ public class ParticleManager : MonoSingleton<ParticleManager>
         Destroy(go.gameObject, e.Duration);
     }
 
-
-
-    //EVENT HANDLING
-    /*
-    //NB: THIS COULD BE A GREAT USE-CASE FOR OBJECT POOLING
-    public void HandleSolutionParticles(object sender, TearEventArgs e)
-    {
-        ParticleSystem toSpawn;
-        switch(e.AffectedTear.TearType)
-        {
-            case TearOperation.ETearType.GOLD:
-                //
-                toSpawn = tearSolutionGold;
-                break;
-            default:
-                //
-                toSpawn = tearSolutionNormal;
-                break;
-        }
-
-        //SPAWN PARTICLES
-        SpawnParticlesAutoDestroy(toSpawn, e.AffectedTear.gameObject.transform.position, "Tear Solution Particles");
-    }
-    */
 }
