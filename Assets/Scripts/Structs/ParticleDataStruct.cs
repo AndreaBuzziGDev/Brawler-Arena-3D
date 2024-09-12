5d4c62025c483b592a7a566dc6326d44ae9354ce
Andrea Buzzi <aby.development@outlook.it>
2024-09-13 01:08:59 +0200

    Created a Struct to handle this complexity

---
+++ Assets/Scripts/Structs/ParticleDataStruct.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ParticleDataStruct
+{
+    public GameObject particle;
+    public float duration = 5.0f;
+}
