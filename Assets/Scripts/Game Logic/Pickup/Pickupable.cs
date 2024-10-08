b8a53e8e3c9cad43374629f787f4bf1c55110680
Andrea Buzzi <aby.development@outlook.it>
2024-10-08 02:18:56 +0200

    Created folders, too tired to work. Created scripts but will work on them in the future

---
+++ Assets/Scripts/Game Logic/Pickup/Pickupable.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        //TODO: CHECK IF COLLIDER IS PLAYER
+        
+    }
+}
