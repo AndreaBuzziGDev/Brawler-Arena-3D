d055ca7fe2b2d0eb5bf7ae28b4e1d1e32b40b072
Andrea Buzzi <aby.development@outlook.it>
2024-10-14 04:47:56 +0200

    Updated content of the script

--- Assets/Scripts/Game Logic/Pickup/Pickupable.cs
+++ Assets/Scripts/Game Logic/Pickup/Pickupable.cs
@@ -7,39 +7,35 @@ using UnityEngine;
 public class Pickupable : MonoBehaviour
 {
     //DATA
+    [SerializeField] PickupController.EPickupTypes pickupType;
     [SerializeField] Boolean enemyPickup = false;
     Boolean isInert = false;
-    
-    //DATA GETTERS
-    public Boolean IsInert { get {return isInert; } }
-    
 
-    //TODO: PICKUPPABLES SHOULD PROBABLY BECOME INERT AFTER PLAYER PICKS THEM UP
-    //
+
+    //LIFECYCLE FUNCTIONS
     void OnEnable()
     {
         isInert = false;
     }
 
 
-
-
     //TRIGGER
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("This is Pickupable Script");
-        if(!IsInert)
+        if(!isInert)
         {
             isInert = true;
             //TODO: SHOULD THIS CHECK FOR PLAYER IN ADVANCE?
             //TODO: SHOULD THIS IGNORE AND SHOULD IT JUST RELY ON COLLISION MATRIX?
             //DISPATCH EVENT
-            EventManager<TriggerInfoEventArgs>.Instance.Notify(this, new(other.gameObject, this.gameObject));
+            EventManager<PickupEventArgs>.Instance.Notify(this, new(this.pickupType));
             
             //ADDITIONAL ACTIONS?
             //TODO: IS THIS SUPPOSED TO IMPLEMENT A DELEGATE OR A SIMILAR FUNCTIONALITY OF SOME SORT?
             //      MOST LIKELY, YES.
             //      ACTUALLY: NO. DOESN'T NEED THAT BECAUSE IN THIS CASE, PICKUPABLES LOGIC WILL BE HANDLED BY A CENTRALIZED SCRIPT WITH EVENT HANDLING.
+            //      GENERIC TRIGGER VOLUME, HOWEVER, SHOULD.
 
         }
     }
