ad530e949e7d885429f05a477fd441a2f1d43150
Andrea Buzzi <aby.development@outlook.it>
2024-11-12 02:20:10 +0100

    Added TODO

--- Assets/Scripts/Game Logic/Entities/Player/PlayerActionController.cs
+++ Assets/Scripts/Game Logic/Entities/Player/PlayerActionController.cs
@@ -18,6 +18,7 @@ public class PlayerActionController : EntityWithAiming
     [Header("Physics Parameters")]
     [SerializeField] float gravityScale = 0.65f;
     [SerializeField] float movementSpeed = 1.0f;
+    //TODO: MOMENTUM?
 
 
 
