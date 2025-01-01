895f37044c0cc30f5dcb392b1d3073b33573a46f
Andrea Buzzi <aby.development@outlook.it>
2025-01-01 01:37:02 +0100

    Commented reference to avoid issues at runtime

--- Assets/Scripts/User Interface/Game UI/UI_GameHUDLeft.cs
+++ Assets/Scripts/User Interface/Game UI/UI_GameHUDLeft.cs
@@ -10,7 +10,7 @@ public class UI_GameHUDLeft : MonoBehaviour
 
     [Header("Inspector References")]
     [SerializeField] private UI_FilledBar healthBar;
-    [SerializeField] private UI_FilledBar shieldBar;
+    //[SerializeField] private UI_FilledBar shieldBar;
 
     // Start is called before the first frame update
     private void Start()
@@ -19,7 +19,7 @@ public class UI_GameHUDLeft : MonoBehaviour
         if (pc != null)
         {
             pc.PlayerHealthAndShield.OnHealthChanged += healthBar.UpdateFill;
-            pc.PlayerHealthAndShield.OnShieldChanged += shieldBar.UpdateFill;
+            //pc.PlayerHealthAndShield.OnShieldChanged += shieldBar.UpdateFill;
         }
     }
 
@@ -28,7 +28,7 @@ public class UI_GameHUDLeft : MonoBehaviour
         if (pc != null)
         {
             pc.PlayerHealthAndShield.OnHealthChanged -= healthBar.UpdateFill;
-            pc.PlayerHealthAndShield.OnShieldChanged -= shieldBar.UpdateFill;
+            //pc.PlayerHealthAndShield.OnShieldChanged -= shieldBar.UpdateFill;
         }
     }
 }
