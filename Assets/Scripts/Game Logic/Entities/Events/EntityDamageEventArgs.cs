5129e2372619873b966229ea28bebfe76fed0ab8
Andrea Buzzi <aby.development@outlook.it>
2025-01-01 02:54:45 +0100

    Updated event body

--- Assets/Scripts/Game Logic/Entities/Events/EntityDamageEventArgs.cs
+++ Assets/Scripts/Game Logic/Entities/Events/EntityDamageEventArgs.cs
@@ -12,13 +12,23 @@ public class EntityDamageEventArgs : EventArgs
     }
     
     //DATA
-    private EDamageType damageType;
-    
+    protected EDamageType damageType;
+    protected float maxFill;
+    protected float currentFill;
+
+
     //DATA GETTERS
     public EDamageType DamageType { get { return damageType; } }
+    public float MaxFill { get { return maxFill; } }
+    public float CurrentFill { get { return currentFill; } }
+    public float PercentFill { get { return (float) (currentFill / maxFill); } }
+
+
 
     //CONSTRUCTOR
-    public EntityDamageEventArgs(EDamageType damageType){
+    public EntityDamageEventArgs(EDamageType damageType, float maxFill, float currentFill){
         this.damageType = damageType;
+        this.maxFill = maxFill;
+        this.currentFill = currentFill;
     }
 }
