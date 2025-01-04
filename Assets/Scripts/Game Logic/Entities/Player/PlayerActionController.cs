2c94b0e0e1e2018eef17e1136ff8a9ae4f318867
Andrea Buzzi <aby.development@outlook.it>
2025-01-04 19:21:00 +0100

    Added Requirement for RB

--- Assets/Scripts/Game Logic/Entities/Player/PlayerActionController.cs
+++ Assets/Scripts/Game Logic/Entities/Player/PlayerActionController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerActionController : EntityWithAiming
 {
     [Header("Inspector References")]
