48f35e6fbeff676cb7a0a2a972c94fd6a7896758
Andrea Buzzi <aby.development@outlook.it>
2025-01-06 01:16:26 +0100

    Not interested in this for the time being

--- Assets/Scripts/Game Logic/Pickup/Pickupable.cs
+++ Assets/Scripts/Game Logic/Pickup/Pickupable.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//TODO: EVOLVE TO HANDLE INSTANT PICKUP VS PROMPTED PICKUP
 public class Pickupable : MonoBehaviour
 {
     [Header("Pickup Properties")]
