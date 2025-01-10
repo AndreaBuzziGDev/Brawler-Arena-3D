39cf5b85d55fe3e6ce044dec553e3cd47013a627
Andrea Buzzi <aby.development@outlook.it>
2025-01-10 01:26:35 +0100

    Added TODO for other classes

--- Assets/Scripts/Main Controllers/Events/ParticleEffectEventArgs.cs
+++ Assets/Scripts/Main Controllers/Events/ParticleEffectEventArgs.cs
@@ -6,6 +6,7 @@ using System;
 public class ParticleEffectEventArgs : EventArgs
 {
     //DATA
+    //TODO: ADOPT THIS SIMPLIFIED SINTAX IN OTHER EVENTS
     public GameObject ToSpawn { get; }
     public Vector3 Position { get; }
     public string Name { get; }
