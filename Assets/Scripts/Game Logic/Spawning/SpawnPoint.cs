87dd115076cb143954a1ab1f1322822ead8ff774
Andrea Buzzi <aby.development@outlook.it>
2025-01-10 01:50:41 +0100

    Added TODO

--- Assets/Scripts/Game Logic/Spawning/SpawnPoint.cs
+++ Assets/Scripts/Game Logic/Spawning/SpawnPoint.cs
@@ -15,7 +15,7 @@ public class SpawnPoint : MonoBehaviour
 
     //DATA
     [Header("Spawn Point Properties")]
-    [SerializeField] SpawnPointType type = SpawnPointType.GROUND;
+    [SerializeField] SpawnPointType type = SpawnPointType.GROUND;//TODO: THIS IS UNUSED. USE IT FOR FUTURE ITERATIONS
     [SerializeField] Color gizmoColor = Color.green;
     [SerializeField] float spawnRadius = 1.0f;
     [SerializeField] bool spawnStrictlyOnRadius = false;
