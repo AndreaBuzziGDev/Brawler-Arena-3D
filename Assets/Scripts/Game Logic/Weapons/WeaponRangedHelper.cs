3d07c58b846165ce6e023c6d942340febd0291b4
Andrea Buzzi <aby.development@outlook.it>
2025-01-18 19:53:32 +0100

    Edited existing code

--- Assets/Scripts/Game Logic/Weapons/WeaponRangedHelper.cs
+++ Assets/Scripts/Game Logic/Weapons/WeaponRangedHelper.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponRangedHelper : MonoBehaviour
+public class WeaponRangedHelper
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //...
+    
+    
+    //...
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
