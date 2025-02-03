6b9acbe3a3305c7fcb61086992742b152e884e55
Andrea Buzzi <aby.development@outlook.it>
2025-02-03 01:19:26 +0100

    Using attributes to later leverage reflection

--- Assets/Scripts/Debug/DebuggerConfig.cs
+++ Assets/Scripts/Debug/DebuggerConfig.cs
@@ -9,12 +9,26 @@ public class DebuggerConfig
     public LogLevel LogLevel;
     
     //DEBUG FLAG
-    //TODO: USE TUPLES OR SOMETHING ELSE TO MAKE THIS MORE SCALABLE
+    [LogTypeField(LogType.DEFAULT)]
     public bool debugDefault = true;
+
+    [LogTypeField(LogType.WEAPON)]
     public bool debugWeapon = false;
+
+    [LogTypeField(LogType.PARTICLE)]
     public bool debugParticle = false;
+
+    [LogTypeField(LogType.SOUND)]
     public bool debugSound = false;
+
+    [LogTypeField(LogType.SPAWNING)]
     public bool debugSpawning = false;
+    
+    [LogTypeField(LogType.PHYSICS)]
+    public bool debugPhysics = false;
+    
+    [LogTypeField(LogType.AI)]
+    public bool debugAI = false;
     
     
 }
