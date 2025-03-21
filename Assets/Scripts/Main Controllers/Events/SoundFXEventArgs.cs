489774dfd1d62428174a305853d42a1bede0d9f4
Andrea Buzzi <aby.development@outlook.it>
2025-03-21 03:12:48 +0100

    Updated class, now dedicated Enum file exists

--- Assets/Scripts/Main Controllers/Events/SoundFXEventArgs.cs
+++ Assets/Scripts/Main Controllers/Events/SoundFXEventArgs.cs
@@ -5,22 +5,13 @@ using System;
 
 public class SoundFXEventArgs : EventArgs
 {
-    //ENUMS
-    public enum EType
-    {
-        UNBOUND,//UNBOUND VALUE - SHOULD USE AS LITTLE AS POSSIBLE
-        A_FX_PLAYER,
-        A_FX_MOB
-    }
-    
-    
     //DATA
-    public EType EventType { get; }
+    public SoundSourceType EventType { get; }
     public AudioClip CarriedAudioClip { get; }
 
 
     //CONSTRUCTOR
-    public SoundFXEventArgs (EType eventType = EType.UNBOUND, AudioClip aClip = null)
+    public SoundFXEventArgs (SoundSourceType eventType = SoundSourceType.UNBOUND, AudioClip aClip = null)
     {
         this.EventType = eventType;
         this.CarriedAudioClip = aClip;
