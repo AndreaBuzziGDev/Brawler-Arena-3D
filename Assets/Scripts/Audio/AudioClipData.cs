f2f290438d1d0438e0ae64e2b187f64f0bd98305
Andrea Buzzi <aby.development@outlook.it>
2025-03-22 22:30:17 +0100

    Updated code to use serialize fields and added public getters

--- Assets/Scripts/Audio/AudioClipData.cs
+++ Assets/Scripts/Audio/AudioClipData.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Audio Clip Data", menuName = "Audio/Clip Data")]
 public class AudioClipData : ScriptableObject
 {
-    public AudioClip clip;
-    public SoundPlaybackMode playbackMode = SoundPlaybackMode.Normal;
-    public float rateLimitTime = 0.1f; //Minimum delay between iterations
-    public int amountLimit = 1;//TODO: MAKE THIS AVAILABLE AS RANGE IN EDITOR
+    //DATA
+    //TODO: ADD HEADERS AND DESCRIPTORS
+    [SerializeField] AudioClip clip;
+    [SerializeField] SoundPlaybackMode playbackMode = SoundPlaybackMode.Normal;
+    [SerializeField] float rateLimitTime = 0.1f; //Minimum delay between iterations
+    [SerializeField] int amountLimit = 1;//TODO: MAKE THIS AVAILABLE AS RANGE IN EDITOR
+    
+    //DATA GETTERS
+    public AudioClip Clip => clip;
+    public SoundPlaybackMode PlaybackMode => playbackMode;
+    public float RateLimitTime => rateLimitTime;
+    public int AmountLimit => amountLimit;
+    
 }
