c06020790e4dc83231b1275681cff32ccce906c6
Andrea Buzzi <aby.development@outlook.it>
2025-04-06 23:52:13 +0200

    Addressing the TODO relative to aiming, proceeding to design gameplay evolution

--- Assets/Scripts/Game Logic/Entities/Enemy/EnemyAiming.cs
+++ Assets/Scripts/Game Logic/Entities/Enemy/EnemyAiming.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //TODO: REFACTOR THIS. THIS WORKS PERFECTLY AS A SUICIDE BOMBER UNIT, BUT THE GAME WILL NEED TO WORK WITH OTHER SOLUTIONS AS WELL.
-public class EnemyAiming : EntityWithAiming
-{
+public class EnemyAiming : EntityWithAiming{
+    
     [Header("Inspector References")]
     [SerializeField] WeaponController enemyWeapon;
+    
+    
+    //TODO: DEVELOP BEHAVIOUR FOR GAINING AIMING
+    //      UNITS SHOULD BE ABLE TO AIM
+    //      THEIR AIM ACQUISITION SHOULD BE BASED ON PARAMETERS THAT ALLOW TO DISCERN WHAT TYPE OF UNIT IS DOING THE AIMING
+    //      BASED ON WHO IS AIMING, BEHAVIOUR CHANGES
+    //      EG:
+    //      SUICIDE BOMBERS SHOULD TRY TO GET CLOSE TO THE TARGET AND SUICIDE BY COLLIDING AGAINST IT
+    //      ENEMIES THAT CAN SHOOT SHOULD TRY TO SHOOT THE TARGET (WHEN ON SIGHT)
+    //      
+    //ADDITIONAL:
+    //      ALL ENEMIES MIGHT BEHAVE AS SUICIDE BOMBERS, WITH LOWERED DAMAGE 
+    //      POSSIBLY WITHOUT INTENTIONALLY ACTING AS SUICIDE UNITS IN REGULAR PLAY
 
 
     //REFERENCE VALIDATION
@@ -22,7 +35,8 @@ public class EnemyAiming : EntityWithAiming
 
     //ON COLLISION
     //TODO: KEEP THIS ONLY FOR ENEMIES THAT ARE SUICIDE BOMBERS
-    //      REGULAR UNITS SHOULD "REACT" BY PUSHING THE PLAYER BACK
+    //      UNITS CAN REACT TO COLLISION
+    //      REGULAR UNITS ARE PUSHED BACK 
     private void OnCollisionEnter(Collision other)
     {
         IHittable hittable = other.gameObject?.GetComponent<IHittable>();
