34d07921bc5ec6bb9c12bc01fde7084ce6774d44
Andrea Buzzi <aby.development@outlook.it>
2025-04-21 01:47:49 +0200

    Added GameState for future usage

---
+++ Assets/Scripts/Main Controllers/GameState.cs
@@ -0,0 +1,15 @@
+// File: EGameState.cs
+public enum EGameState {
+    Start,
+    Playing,
+    Paused,
+    GameOver,
+    Restarting,
+    Quitting,
+    Exiting,
+    // Optional/Future:
+    // Loading,
+    // Cutscene,
+    // Victory,
+    // Debug
+}
